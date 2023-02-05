10cfeb7593e9dcecb9c63e8cee890e01ee992447
Hariharan Sundaram <gautham.apa@gmail.com>
2023-02-04 22:31:19 -0500

    - Added code for benchmark timer - Added insertion sort logic

--- src/main/java/edu/neu/coe/info6205/util/Timer.java
+++ src/main/java/edu/neu/coe/info6205/util/Timer.java
@@ -4,6 +4,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Class which is able to time the running of functions.
@@ -60,9 +61,19 @@ public class Timer {
      */
     public <T, U> double repeat(int n, Supplier<T> supplier, Function<T, U> function, UnaryOperator<T> preFunction, Consumer<U> postFunction) {
         logger.trace("repeat: with " + n + " runs");
-        // FIXME: note that the timer is running when this method is called and should still be running when it returns. by replacing the following code
-         return 0;
-        // END 
+        for(int i=0; i<n; i++) {
+            T suppliedValue = supplier.get();
+            T preProcessedValue = suppliedValue;
+            if(preFunction != null) preProcessedValue = preFunction.apply(suppliedValue);
+            // Time only the processing function
+            if(!running) resume();
+            U processedValue = function.apply(preProcessedValue);
+            pauseAndLap();
+            if(postFunction != null) postFunction.accept(processedValue);
+        }
+        final double result = meanLapTime();
+        resume();
+        return result;
     }
 
     /**
@@ -187,9 +198,7 @@ public class Timer {
      * @return the number of ticks for the system clock. Currently defined as nano time.
      */
     private static long getClock() {
-        // FIXME by replacing the following code
-         return 0;
-        // END 
+        return System.nanoTime();
     }
 
     /**
@@ -200,9 +209,7 @@ public class Timer {
      * @return the corresponding number of milliseconds.
      */
     private static double toMillisecs(long ticks) {
-        // FIXME by replacing the following code
-         return 0;
-        // END 
+        return TimeUnit.NANOSECONDS.toMillis(ticks);
     }
 
     final static LazyLogger logger = new LazyLogger(Timer.class);
